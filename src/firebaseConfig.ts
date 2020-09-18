a3b2e69a92d5358a106491584f477ac9676978de
fdAssis <fd07ass@gmail.com>
2020-09-18 20:20:24 -0300

    modified firebaseConfig

--- src/firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: 'AIzaSyCM16TEh-0aptYSsbBW5wUPqfKLivuHhho',
-  authDomain: 'donateapp-99c9b.firebaseapp.com',
-  databaseURL: 'https://donateapp-99c9b.firebaseio.com',
-  projectId: 'donateapp-99c9b',
-  storageBucket: 'donateapp-99c9b.appspot.com',
-  messagingSenderId: '725479593714',
-  appId: '1:725479593714:web:e6f40584043f0564d1a26b',
-};
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,10 +2,20 @@ import * as firebase from 'firebase';
 import 'firebase/firebase-auth';
 import 'firebase/firebase-firestore';
 
-import firebaseConfig from '../firebaseConfig';
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(config);
 const db = firebaseApp.firestore();
+
+console.log(config);
 
 export default {
   googlePup: async () => {
